e137798a44f471ab401ee26a2eca62e88bba391d
Laurie Cai <laurie801@gmail.com>
2020-12-09 10:49:15 -0800

    Start from scratch - create board array

--- app.js
+++ app.js
@@ -1,47 +1,23 @@
-// players class - generates players
-class Player {
-    constructor(name, marker) {
-        this.name = name;
-        this.marker = marker;
-    }
+// player factory function - generates players
+function newPlayer(name, marker) {
+    return {name, marker};
 }
 
-const playerOne = new Player('Tiffany', 'Bolt');
-const playerTwo = new Player('Laurie', 'Heart');
+const playerOne = newPlayer('Tiffany', 'bolt');
+const playerTwo = newPlayer('Laurie', 'heart');
 
-// UI class - handles UI tasks
-class UI {
+// gameboard object - wraps functions relating to gameboard
+const gameBoard = (() => {
 
-    static showMarker(el) {
-        if (activePlayer == 0) {
-            el.classList.add('bolt');
-            el.classList.add('marked');
-        } else {
-            el.classList.add('heart');
-            el.classList.add('marked');
-        }
+    // create board array
+    let board = [];
+    for (i = 0; i < 9; i++) {
+        board.push('');
     }
 
-    static nextPlayer() {
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-    }
+    // render based on board array
+    
 
-    static showPlayerTurn() {
-        const playerTurn = document.querySelector('.player-name');
-        activePlayer === 0 ? playerTurn.textContent = 'Player 1' : playerTurn.textContent = 'Player 2';
-    }
-}
+})();
 
-// events
-let activePlayer = 0;
-
-document.querySelector('.squares').addEventListener('click', (e) => {
-    // display player marker
-    UI.showMarker(e.target);
-
-    // next player
-    UI.nextPlayer();
-
-    // tell next player to make their move
-    UI.showPlayerTurn();
-})
+// game object - wraps functions relating to game flow
