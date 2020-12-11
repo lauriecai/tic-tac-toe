57644194861eb9acd0a038b15517706f60beae74
Laurie Cai <laurie801@gmail.com>
2020-12-10 21:59:00 -0800

    Organize app.js code into objects

--- app.js
+++ app.js
@@ -1,91 +1,118 @@
-// generate board array
-let board = [];
-for (i = 0; i < 9; i++) {
-    board.push('');
+// player factory function
+const createPlayer = (name, marker) => {
+    return {name, marker};
 }
 
-// display square for each array item
-let squares = document.querySelector('.squares');
+// gameboard object
+const gameBoard = (() => {
 
-board.forEach((item, index) => {
-    const square = document.createElement('div');
-    square.className = 'square';
-    squares.appendChild(square);
-})
+    // generate board array
+    let board = [];
+    for (i = 0; i < 9; i++) {
+        board.push('');
+    }
 
-// create players
-const playerOne = {name: 'Player 1', marker: 'bolt'};
-const playerTwo = {name: 'Player 2', marker: 'heart'};
+    // display square for each array item
+    let squares = document.querySelector('.squares');
 
-// player turn
-let activePlayer = playerOne;
+    board.forEach((item, index) => {
+        const square = document.createElement('div');
+        square.className = 'square';
+        squares.appendChild(square);
+    })
 
-// winning conditions
-const winningAxes = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6],
-  ];
-
-let remainingSpots = 9;
-let playerName = document.querySelector('.player-name');
-let subtext = document.querySelector('.subtext');
-let winnerDeclared = false;
-
-// add event listeners on each square
-Array.from(squares.children).forEach((square, index) => {
-    square.addEventListener('click', () => {
-        // display active player marker
-        square.classList.add(activePlayer.marker);
-        square.setAttribute('data', activePlayer.marker);
-        // update array value to be that of active player
-        board[index] = activePlayer.marker;
-        // remove event listener from the marked index
-        square.style.pointerEvents = 'none';
-        // update remainingSpots
-        remainingSpots -= 1;
-        // check winner: if all 3 values within any of these conditions are ===...
-        checkWinner();
-        // check remaining spots
-        if (winnerDeclared == false) {
-            if (remainingSpots > 0) {
-                alertNextPlayer();
-                nextPlayer();
-            } else if (remainingSpots == 0) {
-                declareTie();
+    // add event listeners on each square
+    Array.from(squares.children).forEach((square, index) => {
+        square.addEventListener('click', () => {
+            // display active player marker
+            square.classList.add(game.activePlayer.marker);
+            square.setAttribute('data', game.activePlayer.marker);
+            // update array value to be that of active player
+            board[index] = game.activePlayer.marker;
+            // remove event listener from the marked index
+            square.style.pointerEvents = 'none';
+            // update remainingSpots
+            game.remainingSpots -= 1;
+            // check winner: if all 3 values within any of these conditions are ===...
+            game.checkWinner();
+            // check remaining spots
+            if (game.winnerDeclared == false) {
+                if (game.remainingSpots > 0) {
+                    game.alertNextPlayer();
+                    game.nextPlayer();
+                } else if (game.remainingSpots == 0) {
+                    game.declareTie();
+                }
             }
-        }
-    })
-});
+        })
+    });
 
-// check winner
-function checkWinner() {
-    winningAxes.forEach((item, index) => { // [0, 1, 2, 3, 4, 5, 6, 7]
-        if (board[item[0]] === activePlayer.marker && board[item[1]] === activePlayer.marker && board[item[2]] === activePlayer.marker) {
-            console.log('winner!');
-            subtext.innerHTML = `<b>${activePlayer.name} wins!</b>`;
-            winnerDeclared = true;
-        } 
-    })
-}
+    // return
+    return {
+        board
+    };
+})();
 
-// next player
-function nextPlayer() {
-    activePlayer === playerOne ? activePlayer = playerTwo : activePlayer = playerOne;
-}
+// game object
+const game = (() => {
 
-// alert next player
-function alertNextPlayer() {
-    activePlayer === playerOne ? playerName.textContent = 'Player 2' : playerName.textContent = 'Player 1';
-}
+    // declare players
+    const playerOne = createPlayer('Player 1', 'bolt');
+    const playerTwo = createPlayer('Player 2', 'heart');
 
-// declare tie
-function declareTie() {
-    console.log("It's a tie");
-    subtext.innerHTML = "<b>Tie game!</b>";
-}
+    // starting point
+    let activePlayer = playerOne;
+    let winnerDeclared = false;
+    let remainingSpots = 9;
+
+    // winning conditions
+    const winningAxes = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],
+    ];
+
+    // check winner
+    function checkWinner() {
+        winningAxes.forEach((item, index) => { // [0, 1, 2, 3, 4, 5, 6, 7]
+            if (gameBoard.board[item[0]] === activePlayer.marker && gameBoard.board[item[1]] === activePlayer.marker && gameBoard.board[item[2]] === activePlayer.marker) {
+                console.log('winner!');
+                subtext.innerHTML = `<b>${activePlayer.name} wins!</b>`;
+                winnerDeclared = true;
+            } 
+        })
+    }
+
+    // alert next player
+    function alertNextPlayer() {
+        let playerName = document.querySelector('.player-name');
+        activePlayer === playerOne ? playerName.textContent = 'Player 2' : playerName.textContent = 'Player 1';
+    }
+
+    // next player
+    function nextPlayer() {
+        activePlayer === playerOne ? activePlayer = playerTwo : activePlayer = playerOne;
+    }
+
+    // declare tie
+    function declareTie() {
+        let subtext = document.querySelector('.subtext');
+        subtext.innerHTML = "<b>Tie game!</b>";
+    }
+
+    // return
+    return {
+        activePlayer,
+        remainingSpots,
+        checkWinner,
+        alertNextPlayer,
+        nextPlayer,
+        declareTie,
+        winnerDeclared
+    };
+})();
